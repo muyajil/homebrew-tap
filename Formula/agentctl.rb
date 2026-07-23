9212075b0687f90ea70b5a98d89f31f14571ef30
Mohammed <mohammed@ajil.ch>
2026-07-23 18:53:56 +0200

    agentctl 2.2.7

--- Formula/agentctl.rb
+++ Formula/agentctl.rb
@@ -13,15 +13,15 @@ end
 class Agentctl < Formula
   desc "Control plane for kernel-isolated AI agents (macOS CLI)"
   homepage "https://github.com/muyajil/agentctl"
-  version "2.2.6"
+  version "2.2.7"
 
   on_arm do
     url "https://agentctl.srv.ajil.ch/bin/agentctl-darwin-arm64", using: NetrcDownloadStrategy
-    sha256 "cfb02627e3b9c41d393b6483d95a9f60d946b0e6dccba40a00241201d1556a9b"
+    sha256 "1e4df35c2b0bb0b0bac45ec4cd67d354e46b05ed406ae2f288871792e58b17a3"
   end
   on_intel do
     url "https://agentctl.srv.ajil.ch/bin/agentctl-darwin-amd64", using: NetrcDownloadStrategy
-    sha256 "39baceb95dfd0b8884724dd7eab56e9b484cfac32d4ff602430d0d3f972c4b19"
+    sha256 "2962ec8c966f1542c04d61efe4c95bddb3cd9ca6d582d4374ac1541f4e3c3c1a"
   end
 
   def install

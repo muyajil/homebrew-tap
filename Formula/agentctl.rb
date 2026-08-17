d83e8d5f958a4054ab355700d40410388bd922b2
agentctl release (noor) <agentctl@ajil.ch>
2026-08-17 16:39:37 +0200

    agentctl 4.5.19

--- Formula/agentctl.rb
+++ Formula/agentctl.rb
@@ -13,15 +13,15 @@ end
 class Agentctl < Formula
   desc "Control plane for kernel-isolated AI agents (macOS CLI)"
   homepage "https://github.com/muyajil/agentctl"
-  version "4.5.18"
+  version "4.5.19"
 
   on_arm do
     url "https://agentctl.srv.ajil.ch/bin/agentctl-darwin-arm64", using: NetrcDownloadStrategy
-    sha256 "12d3c59f6d160eab3728aa008b07882c18f39e8db5c3d4906af12198a2a771ba"
+    sha256 "5d4c9ce6a9c302f7864515e1e9a3bee4e21cd8971e2c04444865c3bbfc9a93c7"
   end
   on_intel do
     url "https://agentctl.srv.ajil.ch/bin/agentctl-darwin-amd64", using: NetrcDownloadStrategy
-    sha256 "12a09d98c724528e095d43c2a6156b292d13da8d3b3ec020fe0f2869eb474fe9"
+    sha256 "fedf538e5c7aa1a32fd5572744dfb2ad5c9eed09c55661d224a554c171447338"
   end
 
   def install
